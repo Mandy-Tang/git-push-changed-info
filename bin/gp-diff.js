dac8fa67d59b85e104d97ea7783b62851f002349
Tang Man <189658646@qq.com>
2020-03-26 20:31:15 +0800

    rename and use the argv

--- bin/gp-diff.js
+++ bin/gp-diff.js
@@ -5,9 +5,13 @@ const shell = require('shelljs');
 main();
 
 async function main() {
+  let testCommand = `ng test --watch=false`;
+  if (process.argv.length === 3) {
+    testCommand = process.argv[2];
+  }
   const changedProjects = await getChangedProjects();
   const result = changedProjects.map(project => {
-    return shell.exec(`ng test ${project.project} --watch=false`);
+    return shell.exec(`${testCommand} ${project.project}`);
   });
   const hasFailure = result.find(result => result.code !== 0);
   if (hasFailure) {
